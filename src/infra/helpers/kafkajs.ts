d445737d8e10dbb3cb2c3a1a70b2e51a50b31234
Julian Campos <julian.campos@minu.co>
2021-01-08 21:02:54 -0300

    removed unnecessary code

--- src/infra/helpers/kafkajs.ts
+++ src/infra/helpers/kafkajs.ts
@@ -31,37 +31,23 @@ export class KafkaHelper implements KafkaContract {
   async receive(topic: string) {
     await this.consumer.connect();
     await this.consumer.subscribe({ topic, fromBeginning: true });
-    let count = 1;
+
     await this.consumer.run({
       autoCommit: false,
-      // eachMessage: async ({ topic, partition, message }) => {
-      //   count++;
-      //   console.log('======== ', count);
-      //   this.consumer.pause([{ topic }])
-      //   //setTimeout(() => this.consumer.resume([{ topic }]), 5000)
-      //   // setTimeout(() => {
-      //   //   console.log(new Date())
-      //   //   console.log('Message: ', message?.value.toString())
-      //   //   console.log(`Received: Topic: ${topic} - Partition: ${partition} - Message: ${ JSON.stringify(message)}`)
-      //   // }, 5000)
-      // }
-
       eachBatchAutoResolve: false,
-
       eachBatch: async ({ batch, resolveOffset, heartbeat, isRunning, isStale }) => {
-        console.log('bbbbbbbb ', batch?.messages.length)
         for (let message of batch.messages) {
-          // console.log({
-          //   topic: batch.topic,
-          //   partition: batch.partition,
-          //   highWatermark: batch.highWatermark,
-          //   message: {
-          //     offset: message.offset,
-          //     //key: message.key.toString(),
-          //     value: message.value.toString(),
-          //     //headers: message.headers,
-          //   }
-          // })
+          console.log({
+            topic: batch.topic,
+            partition: batch.partition,
+            highWatermark: batch.highWatermark,
+            message: {
+              offset: message.offset,
+              key: message?.key.toString(),
+              value: message?.value.toString(),
+              headers: message.headers,
+            }
+          })
 
           resolveOffset(message.offset)
           await heartbeat();
@@ -70,8 +56,8 @@ export class KafkaHelper implements KafkaContract {
             partition: batch.partition,
             offset: message.offset
           }]);
+        }
       }
-    }
     })
     return true;
   }
